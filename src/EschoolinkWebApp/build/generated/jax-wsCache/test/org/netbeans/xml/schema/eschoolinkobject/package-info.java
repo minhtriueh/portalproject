6ea469dcce09773fff3cc0a6c4337a8b3ad6ea3f
duychinhnguyenvn <duychinhnguyenvn@4a22eca7-e0f9-57ca-9faa-d0ee00849ed1>
2010-08-14 09:41:17 +0000

    Xong tương đối template của trang web

--- src/EschoolinkWebApp/build/generated/jax-wsCache/test/org/netbeans/xml/schema/eschoolinkobject/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xml.netbeans.org/schema/EschoolinkObject", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.netbeans.xml.schema.eschoolinkobject;
