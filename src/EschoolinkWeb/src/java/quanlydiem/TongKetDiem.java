da92fdc1405aa23e99d486aa2ac4d2acb81c4bed
nguyentanmo <nguyentanmo@4a22eca7-e0f9-57ca-9faa-d0ee00849ed1>
2010-09-09 10:24:28 +0000

    thêm trang jsp

---
+++ src/EschoolinkWeb/src/java/quanlydiem/TongKetDiem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlydiem;
+
+/**
+ *
+ * @author nguyentanmo
+ */
+public class TongKetDiem {
+
+    public TongKetDiem() {
+    }
+
+    public String execute() throws Exception {
+        return "success";
+    }
+}
