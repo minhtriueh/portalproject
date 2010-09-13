766cd635e88e40690cbdb80d78561313c0d9fbed
huutai1988 <huutai1988@4a22eca7-e0f9-57ca-9faa-d0ee00849ed1>
2010-09-13 03:25:17 +0000

    update source quanlythongtinkhoitao

---
+++ src/EschoolinkWeb/src/java/quanlythongtinkhoitao/TaoQuyen.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package quanlythongtinkhoitao;
+
+/**
+ *
+ * @author GIN
+ */
+public class TaoQuyen {
+ public String execute() throws Exception {
+        return "success";
+    }
+}
