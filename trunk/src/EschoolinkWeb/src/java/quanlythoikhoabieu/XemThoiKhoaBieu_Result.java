cc41208a76b0fc1f0f33e7cdb078c4c986c69655
huutai1988 <huutai1988@4a22eca7-e0f9-57ca-9faa-d0ee00849ed1>
2010-09-13 04:17:23 +0000

    update phan Thoi Khoa Bieu

---
+++ trunk/src/EschoolinkWeb/src/java/quanlythoikhoabieu/XemThoiKhoaBieu_Result.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package quanlythoikhoabieu;
+
+/**
+ *
+ * @author GIN
+ */
+public class XemThoiKhoaBieu_Result {
+
+}
