5b8da4a2ad8ccc19546e62f408f52940b442ed2b
huutai1988 <huutai1988@4a22eca7-e0f9-57ca-9faa-d0ee00849ed1>
2010-09-13 04:17:23 +0000

    update phan Thoi Khoa Bieu

---
+++ src/EschoolinkWeb/src/java/quanlythoikhoabieu/XemThoiKhoaBieu.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package quanlythoikhoabieu;
+
+/**
+ *
+ * @author GIN
+ */
+public class XemThoiKhoaBieu {
+    public String execute() throws Exception{
+        return "success";
+    }
+}
