6ca520d4be8fe3808506a9874bfb66af9284357c
huutai1988 <huutai1988@4a22eca7-e0f9-57ca-9faa-d0ee00849ed1>
2010-09-16 08:19:37 +0000

    update file struts quanlythoikhoabieu

--- src/EschoolinkWeb/src/java/quanlythoikhoabieu/XemThoiKhoaBieu_Result.java
+++ src/EschoolinkWeb/src/java/quanlythoikhoabieu/XemThoiKhoaBieu_Result.java
@@ -4,9 +4,7 @@
  */
 package quanlythoikhoabieu;
 
-import java.math.BigInteger;
 import javax.servlet.http.HttpServletRequest;
-import org.apache.struts2.ServletActionContext;
 import org.netbeans.xml.schema.thoikhoabieu_eschoolinkobject.ThoiKhoaBieu;
 
 /**
@@ -17,73 +15,70 @@ public class XemThoiKhoaBieu_Result {
 
     String tenlop;
     String hocky;
-    String idNamHoc;
+
     ThoiKhoaBieu thoikhoabieu;
 
-    HttpServletRequest request = ServletActionContext.getRequest();
 
     public String execute() throws Exception {
         thoikhoabieu = new ThoiKhoaBieu();
-        BigInteger hk = new BigInteger(hocky);
-        Integer hk2  = new Integer(hocky);
-        Long idnh = new Long(idNamHoc);
 
-        long idLop = callIdLop(tenlop);
+        thoikhoabieu = callService(tenlop, new Integer(hocky));
 
-        thoikhoabieu = callService(tenlop, hk);
-
-        thoikhoabieu.setIdLop(idLop);
-        thoikhoabieu.setHocKy(hk2);
-        thoikhoabieu.setIdNamHoc(idnh);
         thoikhoabieu.getChiTietThoiKhoaBieu();
+       
 
-        
 
         return "success";
     }
 
-    private ThoiKhoaBieu callService(String tenlop, BigInteger hk) {
+    private ThoiKhoaBieu callService(String tenlop, int hocky) {
 
 
         ThoiKhoaBieu tkb = new ThoiKhoaBieu();
 
-
-        try {
-
-            // Call Web Service Operation
+        try { // Call Web Service Operation
             qlthoikhoabieu.QLThoiKhoaBieuService3 service = new qlthoikhoabieu.QLThoiKhoaBieuService3();
             qlthoikhoabieu.XemTKBPortType port = service.getXemTKB();
             // TODO initialize WS operation arguments here
-
             // TODO process result here
-            org.netbeans.xml.schema.thoikhoabieu_eschoolinkobject.ThoiKhoaBieu result = port.xemTKBOperation(tenlop, hk);
-            tkb = result;
-
-            System.out.println("Result = " + tkb);
-
+            org.netbeans.xml.schema.thoikhoabieu_eschoolinkobject.ThoiKhoaBieu result = port.xemTKBOperation(tenlop, hocky);
+            System.out.println("Result = "+result);
+            tkb=result;
         } catch (Exception ex) {
-            System.out.println(ex);
+             System.out.println("Result = "+ex);
         }
+
 
 
         return tkb;
 
     }
 
-    private long callIdLop(String tenlop) {
-        long resultid = -1;
-        try { // Call Web Service Operation
-            qldiemcomapp.QLDiemComAppService38 service = new qldiemcomapp.QLDiemComAppService38();
-            qldiemcomapp.TimIdLopByTenLopPortType port = service.getTimIdLopByTenLop();
-            // TODO initialize WS operation arguments here
-            // TODO process result here
-            long result = port.timIdLopByTenLopOperation(tenlop);
-            resultid = result;
-            System.out.println("Result = " + result);
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
-        return resultid;
 
+    public String getHocky() {
+        return hocky;
     }
+
+    public void setHocky(String hocky) {
+        this.hocky = hocky;
+    }
+
+
+    public String getTenlop() {
+        return tenlop;
+    }
+
+    public void setTenlop(String tenlop) {
+        this.tenlop = tenlop;
+    }
+
+    public ThoiKhoaBieu getThoikhoabieu() {
+        return thoikhoabieu;
+    }
+
+    public void setThoikhoabieu(ThoiKhoaBieu thoikhoabieu) {
+        this.thoikhoabieu = thoikhoabieu;
+    }
+
+
 }
